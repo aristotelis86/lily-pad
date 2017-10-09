d39b886840a40b8c72d8aa4c1640f3c49879d032
Aristotelis Liakatas <agliak@gmail.com>
2017-10-09 14:01:27 +0100

    Update PerformAnalysis.py

--- LilyPad/PostProcessTools/PerformAnalysis.py
+++ LilyPad/PostProcessTools/PerformAnalysis.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 # Use this script to produce graphics and extract information for a specific
 # sheet and/or control point. (Needs modification to call the functions for the 
 # required analysis, example calls are given)
@@ -8,7 +6,7 @@
 import sheetprocessing as myLib
 
 sheetN = 0; # sheet (numbering of sheets from 0)
-pointN = 16; # control point from sheet (numbering of cpoints from 0)
+pointN = 16; # control point from previous sheet (numbering of cpoints from 0)
 
 myLib.read_sheet_info( sheetN ) # just print available information on the sheet
 
@@ -20,15 +18,15 @@ myLib.point_plots( sheetN, pointN ) # create+save plots for position, velocity,
                                     # force and phase space in x,y directions 
                                     # for one control point.
 
-myLib.frequency_plot( sheetN ) # create+save plots of the FFT analysis
-                                       # of the time series from both x and y 
-                                       # position of one control point.
+myLib.frequency_plot( sheetN, pointN=-1 ) # create+save plots of the FFT analysis
+                               # of the time series from both x and y 
+                               # position of one control point.
 
-#myLib.normalMode_frequency_plot( sheetN, pointN, mode=1, stretchRatio=0.011, align="y" )
+myLib.normalMode_frequency_plot( sheetN, pointN=-1, mode=1, stretchRatio=0.1, align="y" )
 # create+save the plot of the FFT analysis of the vibration (pinned-pinned)
 # tracked by one control point. 
                                        
-#myLib.impulse_frequency_analysis( sheetN, newL=44.03, pointN, g=10, align="y" )
+myLib.impulse_frequency_analysis( sheetN, pointN=-1, newL=44.03, pointN, g=10, align="y" )
 # create+save the plot of the FFT analysis of the vibration (pinned-free)
 # tracked by one control point. 
                                        
